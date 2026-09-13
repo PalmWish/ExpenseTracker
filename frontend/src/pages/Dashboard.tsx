6c66eaa1b2ab75082794871666bf65e7dfea9543
Punnawish <177822412+KawMunKaiAroiMak@users.noreply.github.com>
2026-09-13 19:50:34 +0700

    fix: cannot import DashBoardSummary

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import DashBoardSummary from "../components/DashBoardSummary"
+import DashBoardSummary from "../components/DashBoardSummary";
 import ExpenseChart from "../components/ExpenseChart";
 import TransactionFilter from "../components/TransactionFilter";
 import { TransactionList } from "../components/TransactionList";
